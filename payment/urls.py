8f575490ce08028f9661d884eaf7a71e80490c6f
Happy Kumar <happy.kumar@gmail.com>
2023-07-03 10:27:53 +0530

    second commit

--- payment/urls.py
+++ payment/urls.py
@@ -5,6 +5,5 @@ from payment import views
 urlpatterns = [
     path('', views.form, name='form'),
     path('transactions/', views.transactions_list, name='transactions'),
-    path('dependantfield/', views.dependantfield, name='dependantfield'),
 ]
     
--- payment/views.py
+++ payment/views.py
@@ -27,18 +27,3 @@ def transactions_list(request):
     transaction = Transaction.objects.all()
     context = {'transaction':transaction}
     return render(request, 'transactions_list.html', context=context)
-
-
-def dependantfield(request):
-    account_id = request.GET.get('account')
-    selected_account = get_object_or_404(Account, id=account_id) if account_id else None
-
-    transactions = Transaction.objects.filter(account=selected_account) if selected_account else []
-
-    accounts = Account.objects.all()
-    return render(request, 'demo.html', {
-        'accounts': accounts,
-        'selected_account': selected_account,
-        'transactions': transactions,
-    })
-    
